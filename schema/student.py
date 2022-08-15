20996c22a0943efb24d4149b55d405948cc9a104
lakhinsu <hinsulak@gmail.com>
2022-08-15 19:36:37 +0530

    Removed unused function definition

--- schema/student.py
+++ schema/student.py
@@ -2,11 +2,6 @@ from pydantic import BaseModel
 from uuid import UUID, uuid4
 from typing import Optional
 
-
-def generate_uuid():
-    return UUID(str(uuid4()))
-
-
 class StudentSchema(BaseModel):
     """Represents a student model for the student API."""
 
